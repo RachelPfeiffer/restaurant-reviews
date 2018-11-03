2f7ca285bbf38f0f13bd88f4673abd82b31c2bc0
Rachel Pfeiffer <rochelfried9@gmail.com>
2018-11-03 22:53:45 +0200

    added address container

--- js/main.js
+++ js/main.js
@@ -158,27 +158,31 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const name = document.createElement('h1');
+  name.innerHTML = restaurant.name;
+  li.append(name);
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
-  li.append(name);
+  const addressHolder = document.createElement('div');
+  addressHolder.className = 'addressHolder';
+  li.append(addressHolder);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  addressHolder.appendChild(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  addressHolder.appendChild(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  addressHolder.appendChild(more)
 
   return li
 }
