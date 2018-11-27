d4d54d69adcf29fa17a25929f9ba0917137e940f
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-11-27 20:33:42 +0200

    fixed image paths in service worker - worker running successfully!

--- service_worker.js
+++ service_worker.js
@@ -1,34 +1,38 @@
 self.addEventListener('install', function(event) {
-  console.log('installing *thirty-third* SW');
+  console.log('installing *thirty-fifth* SW');
   event.waitUntil(
-    caches.open('reviews-cache-v5').then(function (cache) {
+    caches.open('reviews-cache-v6').then(function (cache) {
 cache.addAll([
   '/',
   'css/detail-page-styles.css',
   'css/main-page-styles.css',
   'css/shared-styles.css',
   'data/restaurants.json',
-  'img/1.jpg',
-  'img/2.jpg',
-  'img/3.jpg',
-  'img/4.jpg',
-  'img/5.jpg',
-  'img/6.jpg',
-  'img/7.jpg',
-  'img/8.jpg',
-  'img/9.jpg',
-  'img/10.jpg',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
   'js/dbhelper.js',
   'js/main.js',
   'js/restaurant_info.js'
 ])  }))
 });
 self.addEventListener('activate', function(event) {
-  console.log('activating *thirty-third* SW');
+  console.log('activating *thirty-fifth* SW');
 });
 
+console.log(caches.match);
+
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch('img/1.jpg')
-  );
+console.log(event.request);
+caches.match(event.request).then(function (response) {
+  if(response) return response;
+  return fetch(event.request);
+});
 });
