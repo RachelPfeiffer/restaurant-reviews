78ee853b58d755433e818c130a6a81f3bbb4335a
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-11-27 12:10:50 +0200

    now responding to fetches with a picture, all assets cached. Go go go!

--- service_worker.js
+++ service_worker.js
@@ -1,12 +1,34 @@
 self.addEventListener('install', function(event) {
-  console.log('installing *twentieth* SW');
+  console.log('installing *thirty-third* SW');
+  event.waitUntil(
+    caches.open('reviews-cache-v5').then(function (cache) {
+cache.addAll([
+  '/',
+  'css/detail-page-styles.css',
+  'css/main-page-styles.css',
+  'css/shared-styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js'
+])  }))
 });
 self.addEventListener('activate', function(event) {
-  console.log('activating *twentieth* SW');
+  console.log('activating *thirty-third* SW');
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    new Response('Lts change the world one step at a time')
-  )
+    fetch('img/1.jpg')
+  );
 });
