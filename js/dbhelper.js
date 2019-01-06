d3606076b9cae47e0df0eced9dfa1a646c6cca5b
Rachel Pfeiffer <rochelfried9@gmail.com>
2019-01-06 21:42:19 +0200

    fixing paths for github pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,9 +8,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8080 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `https://rachelpfeiffer.github.io/restaurant-reviews//data/restaurants.json`;
   }
+  
 
   /**
    * Fetch all restaurants.
